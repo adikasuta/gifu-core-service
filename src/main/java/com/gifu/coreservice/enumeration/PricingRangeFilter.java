e68b4b79fc2cfad2d256570ecc63a91357b65dd4
Adika Suta <adika.suta@online-pajak.com>
2023-04-08 03:04:10 +0700

    fix filter

--- src/main/java/com/gifu/coreservice/enumeration/PricingRangeFilter.java
+++ src/main/java/com/gifu/coreservice/enumeration/PricingRangeFilter.java
@@ -7,8 +7,8 @@ import java.math.BigDecimal;
 @Getter
 public enum PricingRangeFilter {
     BELLOW_10000("< 10.000", BigDecimal.ZERO, BigDecimal.valueOf(10_000)),
-    BETWEEN_10000_to_20000("10.000 ~ 20.000", BigDecimal.valueOf(10_001), BigDecimal.valueOf(20_000)),
-    ABOVE_20000("> 20.000", BigDecimal.valueOf(20_001), null);
+    BETWEEN_10000_to_20000("10.000 ~ 20.000", BigDecimal.valueOf(10_000), BigDecimal.valueOf(20_000)),
+    ABOVE_20000("> 20.000", BigDecimal.valueOf(20_000), null);
 
     private final BigDecimal low;
     private final BigDecimal high;
